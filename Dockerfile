4254e2cc3d511a95c626e0c7f4a583074f1b1710
Akhaladze <4468618@gmail.com>
2026-02-09 21:23:43 +0200

    Sote fixes

--- Dockerfile
+++ Dockerfile
@@ -7,4 +7,8 @@ RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
 
-CMD ["gunicorn", "-w", "4", "-b", "0.0.0.0:5000", "app:app"]
+# CMD ["gunicorn", "-w", "4", "-b", "0.0.0.0:5000", "app:app"]
+# Добавляем --chdir homebot
+CMD ["gunicorn", "--chdir", "homebot", "-w", "4", "-b", "0.0.0.0:5000", "app:app"]
+
+# CMD ["python", "app.py"]
---
+++ doc/ci-tools/docker-build-push.sh
@@ -0,0 +1,12 @@
+# 1. Login to Docker Hub (if not already logged in)
+docker login
+
+# 2. Build the image with the Docker Hub tag
+docker build -t akhaladze/iot-homebot:latest .
+
+# 3. Push the image to Docker Hub
+docker push akhaladze/iot-homebot:latest
+
+
+# 4. compose variant
+docker compose build && docker compose push &&  docker compose up 
